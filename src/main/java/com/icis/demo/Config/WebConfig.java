08fef741ff16133c884d0036ab1646b79475c1b6
laxaris <basrihasan6435@gmail.com>
2024-05-26 00:07:12 +0300

    done

--- src/main/java/com/icis/demo/Config/WebConfig.java
+++ src/main/java/com/icis/demo/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://icis-frontend.vercel.app")
+                .allowedOrigins("https://icis-frontend.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Content-Disposition"); // Expose Content-Disposition header
